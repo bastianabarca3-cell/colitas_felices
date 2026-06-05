fa4ffbd1bbfb771220888064b9092f0e0c4649f1
bastianabarca3-cell <bastianabarca3@gmail.com>
2026-06-04 21:40:37 -0400

    se agrega footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
-let buttonToTop = document.getElementById("btnTop")
+const buttonToTop = document.getElementById("btnTop");
 
 buttonToTop.addEventListener("click", () => {
-    window.scrollTo({top: 0})
-})
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 
 window.addEventListener("scroll", () => {
-    if(window.scrollY >= 200) {
-        buttonToTop.classList.remove("d-none")
+    if (window.scrollY >= 200) {
+        buttonToTop.classList.remove("d-none"); // Mostrar
     } else {
-        buttonToTop.classList.add("d-none")
-
+        buttonToTop.classList.add("d-none"); // Ocultar
     }
-})
+});
